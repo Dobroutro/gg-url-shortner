57e82aa82abd3b66bbb793fc27f5daf1195d3e86
Georgi Gerasimov <gerasimov.georgi@gmail.com>
2016-10-12 12:00:34 +0300

    Update login/register title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>URL Shortener Home</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        URL Shortener Home
                     </a>
                 </div>
 
